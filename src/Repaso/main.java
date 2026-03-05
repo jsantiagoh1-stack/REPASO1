f8faf1bcdb8bee11a79a0b383e8f64806cff2d45
JOSÉ JULIÁN SANTIAGO HERNÁNDEZ <jsantiagoh1@miumg.edu.gt>
2026-03-05 12:44:42 -0600

    PRIMER PROGRAMA

--- src/Repaso/main.java
+++ src/Repaso/main.java
@@ -1,9 +1,23 @@
 package Repaso;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("INGRESE N: ");
+		int n = sc.nextInt();
+		int suma = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			if (i % 4 == 0 ) {
+				suma += i;
+			}
+		}
+		System.out.println("LA SUMA DE DIVISIBLES ENTRE 4 ES: ");
+		sc.close();
 
 	}
 
