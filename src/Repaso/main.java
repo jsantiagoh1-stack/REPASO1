2401b348c81823a3d1b891845c6dbbc06ec261f2
JOSÉ JULIÁN SANTIAGO HERNÁNDEZ <jsantiagoh1@miumg.edu.gt>
2026-03-05 13:02:05 -0600

    PRIMER PROGRAMA

--- src/Repaso/main.java
+++ src/Repaso/main.java
@@ -9,12 +9,14 @@ public class main {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("INGRESE N: ");
 		int n = sc.nextInt();
-		long factorial = 1;
+		int suma = 0;
 		
 		for (int i = 1; i <= n; i++) {
-				factorial *= i;
+			if (i % 4 == 0 ) {
+				suma += i;
+			}
 		}
-		System.out.println("EL FACTORIAL DE " + n + " es: " + factorial);
+		System.out.println("LA SUMA DE DIVISIBLES ENTRE 4 ES: ");
 		sc.close();
 
 	}
